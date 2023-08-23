993375a83479167d1ef73b1411d1d7a07f4d4b06
Abhishek Sharma <akstenx@gmail.com>
2023-08-23 23:06:58 +0530

    recent changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,25 @@
 import ResturantCard from "./ResturantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
 
-   const [listOfResturants, setListOfResturants] = useState(resList);
+   const [listOfResturants, setListOfResturants] = useState([]);
+
+   useEffect(() => {
+      fetchData();
+   }, [])
+
+   const fetchData = async () => {
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.949756&lng=77.6997624&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+      
+      const json = await data.json();
+      console.log(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants)
+      setListOfResturants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+   }
+
+   if(listOfResturants.length === 0) {
+      return <h1>Loading...</h1>
+   }
 
    return (
       <div className="body">
